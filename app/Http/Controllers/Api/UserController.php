aa3b721c32d01fb376321b762f39a967c04e0f06
matteopoliti <matteo.politi17@gmail.com>
2024-06-11 12:03:16 +0200

    Fix: formatted date in api controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ticket;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,6 +12,9 @@ class UserController extends Controller
     public function index()
     {
         $tickets = Ticket::with('operator', 'category')->paginate(6);
+        foreach ($tickets as $ticket) {
+            $ticket->created_at_formatted = Carbon::parse($ticket->created_at)->format('Y-m-d H:i');
+        }
 
         return response()->json([
             'success' => true,
